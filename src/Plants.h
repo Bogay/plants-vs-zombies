4c248ea4e6151ac4ca79404a599907b7fb81234e
yoyoNTNU <yoyo102102102@gmail.com>
2023-06-06 17:40:12 +0800

    add << operator

--- src/Plants.h
+++ src/Plants.h
@@ -3,7 +3,7 @@
 
 
 #include <string>
-
+#include <iostream>
 class Game;
 
 class Plants{
@@ -12,20 +12,21 @@ class Plants{
 
         static void set_gameref(Game &g);
 
-    	  virtual void set_name(string s){};
-    	  virtual void set_max_hp(int i){};
+    	virtual void set_name(string s){};
+    	virtual void set_max_hp(int i){};
         virtual void set_price(int i){};
 
-    	  virtual string get_name()const{};
-    	  virtual int get_max_hp()const{};
+    	virtual string get_name()const{};
+    	virtual int get_max_hp()const{};
         virtual int get_price()const{};
 
         virtual void player_visit(Player &p){};
-        virtual bool zombie_visit(Zombie &z){};//回傳true代表植物還活著
+        virtual bool zombie_visit(Zombie &z){};// return true represent this plant dead
         virtual void healed(){};
 
         int get_cur_hp()const{return cur_hp;}
-
+		
+		
     protected:
         int cur_hp=0;
         static Game *g;
@@ -37,11 +38,11 @@ class CoinPlant: public Plants{
     CoinPlant():cur_hp(max_hp){};
     public:
         virtual void set_name(string s) override;
-    	  virtual void set_max_hp(int i) override;
+    	virtual void set_max_hp(int i) override;
         virtual void set_price(int i) override;
 
-    	  virtual string get_name()const override;
-    	  virtual int get_max_hp()const override;
+    	virtual string get_name()const override;
+    	virtual int get_max_hp()const override;
         virtual int get_price()const override;
 
         virtual void player_visit(Player &p) override;
@@ -55,9 +56,9 @@ class CoinPlant: public Plants{
     private:
         static int each_round_visit;
         static int earn_money;
-    	  static string name;
-      	static int max_hp;
-    	  static int price;
+    	static string name;
+    	static int max_hp;
+    	static int price;
 
     	int cur_visit=0;
 };
@@ -68,11 +69,11 @@ class HornPlant: public Plants{
     HornPlant():cur_hp(max_hp){};
     public:
         virtual void set_name(string s) override;
-    	  virtual void set_max_hp(int i) override;
+    	virtual void set_max_hp(int i) override;
         virtual void set_price(int i) override;
 
-    	  virtual string get_name()const override;
-    	  virtual int get_max_hp()const override;
+    	virtual string get_name()const override;
+    	virtual int get_max_hp()const override;
         virtual int get_price()const override;
 
         virtual void player_visit(Player &p) override;
@@ -84,9 +85,9 @@ class HornPlant: public Plants{
 
     private:
         static int atk;
-    	  static string name;
-    	  static int max_hp;
-    	  static int price;
+    	static string name;
+    	static int max_hp;
+    	static int price;
 };
 
 
@@ -95,11 +96,11 @@ class BombPlant: public Plants{
     BombPlant():cur_hp(max_hp){};
     public:
         virtual void set_name(string s) override;
-    	  virtual void set_max_hp(int i) override;
+    	virtual void set_max_hp(int i) override;
         virtual void set_price(int i) override;
 
-    	  virtual string get_name()const override;
-    	  virtual int get_max_hp()const override;
+    	virtual string get_name()const override;
+    	virtual int get_max_hp()const override;
         virtual int get_price()const override;
 
         virtual void player_visit(Player &p) override;
@@ -107,9 +108,9 @@ class BombPlant: public Plants{
         virtual void healed() override;
 
     private:
-    	  static string name;
-    	  static int max_hp;
-    	  static int price;
+    	static string name;
+    	static int max_hp;
+    	static int price;
 
 };
 
@@ -119,11 +120,11 @@ class HealPlant: public Plants{
     HealPlant():cur_hp(max_hp){};
     public:
         virtual void set_name(string s) override;
-    	  virtual void set_max_hp(int i) override;
+    	virtual void set_max_hp(int i) override;
         virtual void set_price(int i) override;
 
-    	  virtual string get_name()const override;
-    	  virtual int get_max_hp()const override;
+    	virtual string get_name()const override;
+    	virtual int get_max_hp()const override;
         virtual int get_price()const override;
 
         virtual void player_visit(Player &p) override;
@@ -134,9 +135,11 @@ class HealPlant: public Plants{
 
     private:
         static int heal_point;
-    	  static string name;
-    	  static int max_hp;
-    	  static int price;
+    	static string name;
+    	static int max_hp;
+    	static int price;
 };
+
+std::ostream &operator << (std::ostream,const Plants&);
 
 #endif // PLANTS_H
